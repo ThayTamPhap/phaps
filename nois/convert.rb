bb5122631f410e07fd27f5d348a206a56f65f979
Alex <dungtn@gmail.com>
2021-04-23 18:27:08 +0700

    16b

--- nois/convert.rb
+++ nois/convert.rb
@@ -3,5 +3,6 @@ Dir.glob("*.mp3") do |f|
   name = f.gsub(".mp3", "")
   # next if File.size(f) <= 12000000
   next if File.exist?("#{name}.ogg")
-  `sox #{f} -t s24 --rate 44100 -c 1 - | opusenc --downmix-mono --bitrate 13 --raw-rate 44100 --raw-bits 24 --raw-chan 1 - #{name}.ogg`
+  `sox #{f} -t s24 --rate 44100 -c 1 - | opusenc --downmix-mono --bitrate 16 --raw-rate 44100 --raw-bits 24 --raw-chan 1 - #{name}.ogg`
 end
+# ffmpeg -i Noi-Oan-Corona-v2a.m4a -acodec libmp3lame -ab 197k Noi-Oan-Corona-v2a.mp3
--- nois/crawl.rb
+++ nois/crawl.rb
@@ -40,7 +40,7 @@ phap[22] = ['Niệm gì (Hải Yến)', 'https://sutamphap.com/wp-content/upload
 phap[23] = ['Đừng vội vàng (Hải Yến)', 'https://sutamphap.com/wp-content/uploads/2021/02/Dung-voi-vang.mp3']
 phap[24] = ['Biết và thấy (Ngọc Bảo)', 'https://sutamphap.com/wp-content/uploads/2021/02/Biet-va-thay-mix.mp3']
 phap[25] = ['Vô cảm (Hải Yến)', 'https://sutamphap.com/wp-content/uploads/2021/02/Vo-cam.mp3']
-phap[26] = ['Hãy cười vui lên (Giọng đọc Hải Yến)', 'https://sutamphap.com/wp-content/uploads/2021/02/Hay-cuoi-vui-len.mp3']
+phap[26] = ['Hãy cười vui lên (Hải Yến)', 'https://sutamphap.com/wp-content/uploads/2021/02/Hay-cuoi-vui-len.mp3']
 phap[27] = ['Chánh niệm khó quá (Hải Yến)', 'https://sutamphap.com/wp-content/uploads/2021/02/Chanh-niem-kho-qua-mix.mp3']
 phap[28] = ['Chỉ tồn tại trong suy nghĩ (Hải Yến)', 'https://sutamphap.com/wp-content/uploads/2021/02/Chi-ton-tai-trong-suy-nghi-mix.mp3']
 phap[29] = ['Tâm và pháp  (Ngọc Bảo)', 'https://sutamphap.com/wp-content/uploads/2021/02/Tam-va-phap-mix.mp3']
@@ -49,7 +49,7 @@ phap[31] = ['Giai đoạn tu tập khó khăn (Ngọc Bảo)', 'https://sutampha
 phap[32] = ['Chánh niệm chỉ là phương tiện (Hải Yến)', 'https://sutamphap.com/wp-content/uploads/2021/02/Chanh-niem-chi-la-phuong-tien-ghep.mp3']
 phap[33] = ['Cai nghiện (Hải Yến)', 'https://sutamphap.com/wp-content/uploads/2021/02/CaiNghien-ThuNoi-HaiYen.mp3']
 phap[34] = ['Sợ thay đổi (Ngọc Bảo)', 'https://sutamphap.com/wp-content/uploads/2021/01/SOTHAYDOI.mp3']
-phap[35] = ['Chuyện ăn uống đúng (Giọng đọc Ngọc Bảo)', 'https://sutamphap.com/wp-content/uploads/2021/01/CHUYENANUONGDUNG.mp3']
+phap[35] = ['Chuyện ăn uống đúng (Ngọc Bảo)', 'https://sutamphap.com/wp-content/uploads/2021/01/CHUYENANUONGDUNG.mp3']
 phap[36] = ['Cho tất cả các học trò đến tu học với Thầy (Tố Anh)', 'https://sutamphap.com/wp-content/uploads/2021/01/Cho-Tat-Ca-Cac-Hoc-Tro-Den-Tu-Hoc-Voi-Thay-v2a.mp3']
 phap[37] = ['Định vị tương lai (Ngọc Bảo)', 'https://sutamphap.com/wp-content/uploads/2021/01/DINHVITUONGLAI.mp3']
 phap[38] = ['Sống thuận Pháp như thế nào (Ngọc Bảo)', 'https://sutamphap.com/wp-content/uploads/2020/12/SONGTHUANPHAPNHUTHENAO.mp3']
@@ -95,10 +95,97 @@ phap[77] = ['Đừng vội vàng (Tố Anh)', 'https://sutamphap.com/wp-content/
 phap[78] = ['Cai nghiện (Tố Anh)', 'https://sutamphap.com/wp-content/uploads/2020/06/thuyetPhap_CaiNghien.m4a']
 phap[79] = ['Vượt qua dễ duôi (Tố Anh)', 'https://sutamphap.com/wp-content/uploads/2020/06/Vuot-qua-de-duoi.m4a']
 
-for i in 1..79 do
-# `wget #{phap[i][1]}`
-	name = phap[i][1].split('/')[-1].sub(/\..+/,'')
-	next unless File.exist?("#{name}.ogg") && File.size("#{name}.ogg") > 0
-	#print "{\"title\":\"#{phap[i][0]}\",\"audio\":\"nois/#{name}.ogg?#{phap[i][1]}\"},"
-	puts "\"nois/#{name}.ogg\" to \"#{phap[i][0]}\","
+# phaps = []
+# phap.sort_by { |x| x ? x[0] : "" }.each{ |x|
+# 	phaps.push({title: x[0], mp3_url: x[1]}) if x != nil
+# }
+# puts phaps.join(",\n")
+phaps = [
+{:title=>"Biết và thấy (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/02/Biet-va-thay-mix.mp3"},
+# {:title=>"Biết và thấy (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/12/Biet-va-Thay-v2a.mp3"},
+{:title=>"Buông bỏ phiền não (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/06/Buong-bo-phien-nao.mp3"},
+{:title=>"Băn khoăn cuộc sống (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/12/Ban-khoan-cuoc-song.mp3"},
+{:title=>"Bơ vơ (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/04/Bo-vo.mp3"},
+# {:title=>"Bơ vơ (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/12/Bo-Vo-v2a.mp3"},
+{:title=>"Bạn đạo và hội nhóm Phật tử", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/10/Hoi-Nhom-Phat-Tu.mp3"},
+{:title=>"Bản chất của cuộc sống (Ngọc bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/03/Ban-chat-cua-cuoc-song.mp3"},
+# {:title=>"Bản chất của cuộc sống (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/10/Ban-Chat-Cua-Cuoc-Song-v2a.mp3"},
+{:title=>"Bức thư không muốn gửi (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/12/Buc-thu-khong-muon-gui.mp3"},
+{:title=>"Cai nghiện (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/02/CaiNghien-ThuNoi-HaiYen.mp3"},
+# {:title=>"Cai nghiện (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/06/thuyetPhap_CaiNghien.m4a"},
+# {:title=>"Cho tất cả các học trò đến tu học với Thầy (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/03/Cho-tat-ca-hoc-tro-den-tu-hoc-voi-thay.mp3"},
+# {:title=>"Cho tất cả các học trò đến tu học với Thầy (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/01/Cho-Tat-Ca-Cac-Hoc-Tro-Den-Tu-Hoc-Voi-Thay-v2a.mp3"},
+{:title=>"Chuyện ăn uống đúng (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/01/CHUYENANUONGDUNG.mp3"},
+{:title=>"Chánh niệm chỉ là phương tiện (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/02/Chanh-niem-chi-la-phuong-tien-ghep.mp3"},
+# {:title=>"Chánh niệm chỉ là phương tiện (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/07/Chanh-niem-chi-la-phuong-tien.mp3"},
+{:title=>"Chánh niệm khó quá (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/02/Chanh-niem-kho-qua-mix.mp3"},
+# {:title=>"Chánh niệm khó quá (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/07/Chanh-niem-kho-qua.mp3"},
+{:title=>"Chánh niệm thanh lọc thân tâm (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/12/Chanh-niem-thanh-loc-than-tam.mp3"},
+{:title=>"Chỉ tồn tại trong suy nghĩ (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/02/Chi-ton-tai-trong-suy-nghi-mix.mp3"},
+# {:title=>"Chỉ tồn tại trong suy nghĩ (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/09/Chi-Ton-Tai-Trong-Suy-Nghi-v2a.mp3"},
+{:title=>"Cuộc đời bị đánh cắp (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/11/CuocDoiBiDanhCap.mp3"},
+# {:title=>"Cách thực hành thiền Chánh niệm", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/10/Cach-Thuc-Hanh-Thien-Chanh-Niem.mp3"},
+{:title=>"Cách thực hành thiền chánh niệm (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/04/Cach-thuc-hanh-thien-chanh-niem.mp3"},
+{:title=>"Giai đoạn tu tập khó khăn (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/04/Giai-doan-tu-tap-kho-khan.mp3"},
+# {:title=>"Giai đoạn tu tập khó khăn (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/02/Giai-doan-tu-tap-kho-khan-mix.mp3"},
+# {:title=>"Giai đoạn tu tập khó khăn (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/09/Giai-Doan-Tu-Tap-Kho-Khan-v2a.mp3"},
+{:title=>"Góc khuất xấu xí (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/03/Goc-khuat-xau-xi.mp3"},
+# {:title=>"Góc khuất xấu xí (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/11/Goc-Khuat-Xau-Xi-v2a.mp3"},
+{:title=>"Hãy cười vui lên (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/02/Hay-cuoi-vui-len.mp3"},
+# {:title=>"Hãy cười vui lên (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/06/Hay-cuoi-vui-len.mp3"},
+{:title=>"Lo lắng (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/04/Lo-Lang.mp3"},
+# {:title=>"Lo lắng (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/12/Lo-Lang-v2a.mp3"},
+{:title=>"Mấy lời chia sẻ cho người muốn xuất gia (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/03/May-loi-chia-se-cho-nguoi-muon-xuat-gia.mp3"},
+# {:title=>"Mấy lời chia sẻ cho người muốn xuất gia (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/09/May-Loi-Chia-Se-Cho-Nguoi-Muon-Xuat-Gia-v1c.mp3"},
+{:title=>"Một ngày mai chưa hề biết (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/03/Mot-ngay-mai-chua-he-biet.mp3"},
+# {:title=>"Một ngày mai chưa hề biết (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/10/Mot-Ngay-Mai-Chua-He-Biet-v2b.mp3"},
+{:title=>"Một số nguyên lý thiền (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/02/Mot-so-nguyen-ly-thien-mix.mp3"},
+# {:title=>"Một số nguyên lý thiền (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/04/Mot-so-nguyen-ly-thien.mp3"},
+# {:title=>"Một số nguyên lý thiền (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/08/Mot-so-nguyen-ly-thien.mp3"},
+{:title=>"Nhật ký chánh niệm", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/10/NhatKyChanhNiem.mp3"},
+{:title=>"Niệm gì (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/03/Niem-gi.mp3"},
+# {:title=>"Niệm gì (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/06/Niem-gi.m4a"},
+{:title=>"Nỗi oan của Corona (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/07/Noi-Oan-Corona-v2a.m4a"},
+{:title=>"Nửa đạo nửa đời", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/08/Nua-Dao-Nua-Doi-v2a.mp3"},
+{:title=>"Sao chẳng hết sân (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/03/Sao-chang-het-san.mp3"},
+# {:title=>"Sao chẳng hết sân (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/12/Sao-Chang-Het-San-v2a.mp3"},
+{:title=>"Sao khó chịu chẳng chịu đi (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/04/Sao-kho-chiu-chang-chiu-di.mp3"},
+{:title=>"Sân và cách vượt qua (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/03/San-va-cach-vuot-qua.mp3"},
+# {:title=>"Sân và cách vượt qua (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/10/San-va-Cach-Vuot-Qua-v2a.mp3"},
+{:title=>"Sống thuận Pháp như thế nào (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/12/SONGTHUANPHAPNHUTHENAO.mp3"},
+{:title=>"Sợ thay đổi (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/01/SOTHAYDOI.mp3"},
+# {:title=>"Sự thật về Khổ (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/07/Su-that-ve-kho.mp3"},
+{:title=>"Sự thật về khổ (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/03/Su-that-ve-kho.mp3"},
+{:title=>"Tu ở chùa và ở đời (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/04/Tu-o-chua-va-o-doi.mp3"},
+# {:title=>"Tu ở chùa và ở đời (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/09/Tu-O-Chua-Va-O-Doi.mp3"},
+{:title=>"Tâm rộng mở (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/03/Tam-rong-mo.mp3"},
+# {:title=>"Tâm rộng mở (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/10/Tam-Rong-Mo-v2a.mp3"},
+# {:title=>"Tâm và Pháp", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/08/Tam-va-Phap-edited.mp3"},
+{:title=>"Tâm và pháp (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/02/Tam-va-phap-mix.mp3"},
+{:title=>"Tình duyên", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/11/Tinh-Duyen.mp3"},
+{:title=>"Tính cách có cần sửa không (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/03/TInh-cach-co-can-sua-khong.mp3"},
+# {:title=>"Tính cách có cần sửa không (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/04/TInh-cach-co-can-sua-khong-1.mp3"},
+# {:title=>"Tính cách có cần sửa không (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/09/Tinh-Cach-Co-Can-Sua-Khong-v2b.mp3"},
+{:title=>"Tại sao khó chịu chẳng chịu đi (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/12/Tai-Sao-Kho-Chiu-Chang-Chiu-Di-v2a.mp3"},
+{:title=>"Vô cảm (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/02/Vo-cam.mp3"},
+# {:title=>"Vô cảm (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/04/Vo-cam.mp3"},
+# {:title=>"Vô cảm (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/07/Vo-cam.mp3"},
+{:title=>"Vượt qua dễ duôi (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/03/Vuot-qua-de-duoi.mp3"},
+# {:title=>"Vượt qua dễ duôi (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/06/Vuot-qua-de-duoi.m4a"},
+{:title=>"Xuất gia hay trốn đời (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/03/Xuat-gia-hay-tron-doi.mp3"},
+# {:title=>"Xuất gia hay trốn đời (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/08/Xuat-Gia-Hay-Tron-Doi-v2a.mp3"},
+{:title=>"Yêu và đau khổ", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/10/YeuVaDauKho.mp3"},
+{:title=>"Đấu tranh với cuộc sống (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/12/DAUTRANHVOICUOCSONG.mp3"},
+{:title=>"Định nghĩa chánh niệm (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/11/Dinh-Nghia-Chanh-Niem-v2a.mp3"},
+{:title=>"Định vị tương lai (Ngọc Bảo)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/01/DINHVITUONGLAI.mp3"},
+{:title=>"Đừng vội vàng (Hải Yến)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2021/02/Dung-voi-vang.mp3"}
+# {:title=>"Đừng vội vàng (Tố Anh)", :mp3_url=>"https://sutamphap.com/wp-content/uploads/2020/06/Dung-Voi-Vang.m4a"}
+]
+phaps.each do |phap|
+	media = phap[:mp3_url].split('/')[-1] 
+	name = media.sub(/\..+/,'')
+	# `wget #{phap[:mp3_url]}` unless File.exist?(media) && File.size(media) > 0
+	# next unless File.exist?("#{name}.ogg") && File.size("#{name}.ogg") > 0
+	# print "{\"title\":\"#{phap[:title]}\",\"audio\":\"nois/#{name}.ogg?#{phap[:mp3_url]}\"},"
+	puts "\"nois/#{name}.ogg\" to \"#{phap[:title]}\","
 end
