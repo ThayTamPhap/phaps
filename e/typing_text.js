445826e3084a037704e6b79c372331b8e90993cb
Alex Nguyen <tiendung@users.noreply.github.com>
2021-05-22 11:37:58 +0700

    make use of e/typing_text.js

--- e/typing_text.js
+++ e/typing_text.js
@@ -1,19 +1,20 @@
 function normalizeText(value) {
+  value = convertShortcuts(value);
   value = value.replace("...","…").replace(" \""," “").replace("\" ","”");
   value = value.replace(/[[{(]\s+/g, x => " "+x.replace(/\s+/g,""));
   value = value.replace(/\s+[\]})\,\.;:?\!…]/g, x => x.replace(/\s+/g,"")+" ");
   value = value.replace(/\s+[\]})\,\.;:?\!…]/g, x => x.replace(/\s+/g,"")+" ");
   value = value.replace(/^\s+/,"").replace(/\s+$/,"");
-  return  value.replace(/\s+/g," ");
+  value = value.replace(/\s+/g," ");
+  return value;
 }
-console.assert(normalizeText("  d  . ") === "d.");
-console.assert(normalizeText("  d { d f   fd !}  d ,   f .   ") === "d {d f fd!} d, f.");
 
 function convertShortcuts(txt) {
   return txt = txt.replace(typingShortcutsRegex, x => {
     var splits = x.split(/(\s?)(.+)/);
     // console.log("Found:", x, splits);
-    return splits[1] + typingShortcuts[splits[2]];
+    return splits[1] + splits[2][0] + 
+      typingShortcuts[splits[2].toLowerCase()].substr(1,);
   });
 }
 
@@ -37,20 +38,13 @@ ntn như thế nào
 nx  những
 vq  vượt qua`.split("\n").map(x => {
 
-  let temp = x.split(/^(\S+)\s+/);
-  // console.log(temp);
-  if (temp.length === 1) return "-=-=-";
+  let splits = x.split(/^(\S+)\s+/);
+  // console.log(splits);
+  if (splits.length < 2) return "-=-=-"; // any nonsense string
+  typingShortcuts[splits[1]] = splits[2];
+  return splits[1].replace("?", "\\?");
 
-  let k = temp[1];
-  let v = temp[2];
-  typingShortcuts[k] = v;
-
-  k[0] = k[0].toLocaleUpperCase();
-  v[0] = v[0].toLocaleUpperCase();
-  typingShortcuts[k] = v;
-
-  return k.replace("?", "\\?");
-}).slice(1,).join("|")+')(?=\\s|$)', 'gi'); 
+}).slice(1,).join("|")+')(?=[\\s!?.,;:|\\]})]|$)', 'gi'); 
 /* 
 ?= is a positive lookahead
 https://stackoverflow.com/questions/1570896/what-does-mean-in-a-regular-expression
@@ -62,4 +56,7 @@ or followed by another word character (so .\b. is equal to \W\w and \w\W).
 And \w is defined as [A-Za-z0-9_]. So \w doesn’t match greek characters.
 */
 console.assert(convertShortcuts('bg')==='bây giờ');
-console.assert(convertShortcuts('bg chúng ta nc về nx cn đang ở đây')==='bây giờ chúng ta nói chuyện về những con người đang ở đây');
+console.assert(convertShortcuts('Bg chúng ta nc về nx cn đang ở đây')==='Bây giờ chúng ta nói chuyện về những con người đang ở đây');
+
+console.assert(normalizeText("  d  . ") === "d.");
+console.assert(normalizeText("  d { d f   fd !}  d ,   f .   ") === "d {d f fd!} d, f.");
