57f1c350d979d559f2f38c2cb06ca776bb74f684
Alex Nguyen <tiendung@users.noreply.github.com>
2021-05-24 10:56:09 +0700

    more shortscuts

--- e/shortcuts.js
+++ e/shortcuts.js
@@ -1,118 +1,122 @@
+
 /**/_shortcuts = `
-\\d+d 	\\d+ ngày
-\\d+h 	\\d+ giờ
-\\d+ng 	\\d+ người
-\\d+o 	\\d+ tuổi
-\\d+p 	\\d+ phút
-\\d+t 	\\d+ tiếng
-\\d+y 	\\d+ năm
-P       Tăng
-Ph      Pháp
-T       Tăng
-th      thầy
-Th      Thầy
-TB      Tam Bảo
-cT      chư Tăng
-dd      đắc  đạo
-gtt     giải thoát
-gth     giải thích
-xd      xứng đáng
-vn      Việt Nam
-dt      đối tượng
-pv      phục vụ
-kbg     không bao giờ
-trtg    truyền thống
-trth    truyền thừa
-ad 	Ấn Độ
-bog  	bao giờ
-bag     bao giờ
-bt 	bình thường
-bv 	bởi vì
-bvl 	bởi vì là
-byg  	bây giờ
-cd 	cúng dường
-ck 	chánh kiến
-cl 	chất lượng
-cn 	chánh niệm
-blm 	ba-la-mật
-cng  	con người
-cnt 	con người ta  
-cs 	cuộc sống
-cta 	chúng ta
-ctt 	chánh tinh tấn
-cvt 	chính vì thế
-cx  	cũng
-cđ 	cuộc đời
-d?  	đúng không?
-d?, 	đúng không,
-d?. 	đúng không.
-dc  	được
-dd  	dễ duôi
-dg  	đúng
-dk  	điều kiện
-dp  	Đức Phật
-h?  	hiểu không?
-h?, 	hiểu không,
-h?. 	hiểu không.
-hk  	hiểu không
-hl 	hoặc là
-hn 	Hà Nội
-k   	không
-kc 	kích cỡ
-kd  	không được
-kdj 	không được
-kds 	không đúng
-kh 	không hiểu
-kn 	không nói
-ko  	không
-kp 	không phải
-kth	kích thước
-kti 	kiếm tiền
-ktr	kiểm tra
-lbv 	là bởi vì
-lp 	làm phước
-lpcd 	làm phước cúng dường
-lv 	làm việc
-mng 	mọi người
-nc  	nói chuyện
-ng  	người
-ngt 	người ta
-ngta 	người ta
-nh 	nhưng
-nm 	nhưng mà
-nt 	như thế
-nta 	người ta
-m 	mình
-ntn 	như thế nào
-ntno 	như thế nào
-ntny 	như thế này
-nts 	nên tại sao
-nv 	như vậy
-trc 	trước
-nvl 	như vậy là
-nx 	những
-pb 	phước báu
-pt 	phát triển
-qd 	quy định
-qtd 	quyết định
-qđ 	quyết định
-rl  	rất là
-rln 	rất là nhiều
-rn 	rất nhiều
-sp 	số phận
-tg  	thời gian
-tn 	thế nào
-tnm 	thế nhưng mà
-ts 	tại sao
-tst 	tính toán
-tt  	tinh tấn
-ttj 	tu tập
-ttp 	tu tập
-vd 	ví dụ
-vdn 	ví dụ như
-vdnl 	ví dụ như là
-vq  	vượt qua
-vt  	vì thế
-vđ 	vấn đề
-đ   	đúng đắn
-đđ	|||`
+\\d+d    \\d+ ngày
+\\d+h    \\d+ giờ
+\\d+ng   \\d+ người
+\\d+o    \\d+ tuổi
+\\d+p    \\d+ phút
+\\d+t    \\d+ tiếng
+\\d+y    \\d+ năm
+ad    Ấn Độ
+bag   bao giờ
+blm   ba-la-mật
+bog   bao giờ
+bt    bình thường
+bv    bởi vì
+bvl   bởi vì là
+byg   bây giờ
+cd    cúng dường
+ck    chánh kiến
+cl    chất lượng
+cn    chánh niệm
+cng   con người
+cnt   con người ta  
+cs    cuộc sống
+cT    chư Tăng
+cta   chúng ta
+ctt   chánh tinh tấn
+cvt   chính vì thế
+cx    cũng
+cđ    cuộc đời
+d?    đúng không?
+d?,   đúng không,
+d?.   đúng không.
+dc    được
+dd    dễ duôi
+dd    đắc  đạo
+dg    đúng
+dk    điều kiện
+dp    Đức Phật
+dt    đối tượng
+gth   giải thích
+gtt   giải thoát
+h?    hiểu không?
+h?,   hiểu không,
+h?.   hiểu không.
+hk    hiểu không
+hl    hoặc là
+hn    Hà Nội
+k     không
+kbg   không bao giờ
+kc    kích cỡ
+kd    không được
+kdj   không được
+kds   không đúng
+kh    không hiểu
+khkh  khó khăn
+kk    khó khăn
+kn    không nói
+ko    không
+kp    không phải
+kt    kích thước
+kth   kích thước
+kti   kiếm tiền
+ktr   kiểm tra
+lbv   là bởi vì
+lp    làm phước
+lpcd  làm phước cúng dường
+lv    làm việc
+m     mình
+mng   mọi người
+n     nhưng
+nc    nước
+ng    người
+ngt   người ta
+ngta  người ta
+nh    nhưng
+nm    nhưng mà
+nt    như thế
+nta   người ta
+ntn   như thế nào
+ntno  như thế nào
+ntny  như thế này
+nts   nên tại sao
+nv    như vậy
+nvl   như vậy là
+nx    những
+P     Tăng
+pb    phước báu
+Ph    Pháp
+pt    phát triển
+pv    phục vụ
+qd    quy định
+qtd   quyết định
+qđ    quyết định
+rl    rất là
+rln   rất là nhiều
+rn    rất nhiều
+sp    số phận
+T     Tăng
+TB    Tam Bảo
+tg    thời gian
+th    thầy
+tn    thế nào
+tnm   thế nhưng mà
+trc   trước
+trtg  truyền thống
+trth  truyền thừa
+ts    tại sao
+tst   tính toán
+tt    tinh tấn
+ttj   tu tập
+ttp   tu tập
+vd    ví dụ
+vdn   ví dụ như
+vdnl  ví dụ như là
+vn    Việt Nam
+vq    vượt qua
+vt    vì thế
+vđ    vấn đề
+xd    xứng đáng
+đ     đúng đắn
+đđ |||`
