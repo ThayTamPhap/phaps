408d60cc774fbcf2a3208e7a5cad254dbea58a4e
Alex Nguyen <tiendung@users.noreply.github.com>
2021-05-24 08:02:28 +0700

    Update shortcuts.js

--- e/shortcuts.js
+++ e/shortcuts.js
@@ -6,25 +6,27 @@
 \\d+p 	\\d+ phút
 \\d+t 	\\d+ tiếng
 \\d+y 	\\d+ năm
-ad 		Ấn Độ
+vn      Việt Nam
+pv      phục vụ
+ad 	Ấn Độ
 bog  	bao giờ
-bt 		bình thường
-bv 		bởi vì
+bt 	bình thường
+bv 	bởi vì
 bvl 	bởi vì là
 byg  	bây giờ
-cd 		cúng dường
-ck 		chánh kiến
-cl 		chất lượng
-cn 		chánh niệm
+cd 	cúng dường
+ck 	chánh kiến
+cl 	chất lượng
+cn 	chánh niệm
 blm 	ba-la-mật
 cng  	con người
 cnt 	con người ta  
-cs 		cuộc sống
+cs 	cuộc sống
 cta 	chúng ta
 ctt 	chánh tinh tấn
 cvt 	chính vì thế
 cx  	cũng
-cđ 		cuộc đời
+cđ 	cuộc đời
 d?  	đúng không?
 d?, 	đúng không,
 d?. 	đúng không.
@@ -37,64 +39,64 @@ h?  	hiểu không?
 h?, 	hiểu không,
 h?. 	hiểu không.
 hk  	hiểu không
-hl 		hoặc là
-hn 		Hà Nội
+hl 	hoặc là
+hn 	Hà Nội
 k   	không
-kc 		kích cỡ
+kc 	kích cỡ
 kd  	không được
 kdj 	không được
 kds 	không đúng
-kh 		không hiểu
-kn 		không nói
+kh 	không hiểu
+kn 	không nói
 ko  	không
-kp 		không phải
-kth		kích thước
+kp 	không phải
+kth	kích thước
 kti 	kiếm tiền
-ktr		kiểm tra
+ktr	kiểm tra
 lbv 	là bởi vì
-lp 		làm phước
+lp 	làm phước
 lpcd 	làm phước cúng dường
-lv 		làm việc
+lv 	làm việc
 mng 	mọi người
 nc  	nói chuyện
 ng  	người
 ngt 	người ta
 ngta 	người ta
-nh 		nhưng
-nm 		nhưng mà
-nt 		như thế
+nh 	nhưng
+nm 	nhưng mà
+nt 	như thế
 nta 	người ta
-m 		mình
+m 	mình
 ntn 	như thế nào
 ntno 	như thế nào
 ntny 	như thế này
 nts 	nên tại sao
-nv 		như vậy
+nv 	như vậy
 trc 	trước
 nvl 	như vậy là
-nx 		những
-pb 		phước báu
-pt 		phát triển
-qd 		quy định
+nx 	những
+pb 	phước báu
+pt 	phát triển
+qd 	quy định
 qtd 	quyết định
-qđ 		quyết định
+qđ 	quyết định
 rl  	rất là
 rln 	rất là nhiều
-rn 		rất nhiều
-sp 		số phận
+rn 	rất nhiều
+sp 	số phận
 tg  	thời gian
-tn 		thế nào
+tn 	thế nào
 tnm 	thế nhưng mà
-ts 		tại sao
+ts 	tại sao
 tst 	tính toán
 tt  	tinh tấn
 ttj 	tu tập
 ttp 	tu tập
-vd 		ví dụ
+vd 	ví dụ
 vdn 	ví dụ như
 vdnl 	ví dụ như là
 vq  	vượt qua
 vt  	vì thế
-vđ 		vấn đề
+vđ 	vấn đề
 đ   	đúng đắn
-đđ		|||`
+đđ	|||`
