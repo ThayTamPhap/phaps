d866dc13debac8e5d213fc84504971662fe3de39
Alex Nguyen <tiendung@users.noreply.github.com>
2021-05-26 06:27:07 +0700

    4s shortcuts

--- e/shortcuts.js
+++ e/shortcuts.js
@@ -4,6 +4,8 @@
 1s    một số
 1t    một thời
 1tg   một thời gian
+2lct  hai là chúng ta
+??    [??]
 \\d+d    \\d+ ngày
 \\d+h    \\d+ giờ
 \\d+ng   \\d+ người
@@ -11,23 +13,40 @@
 \\d+p    \\d+ phút
 \\d+t    \\d+ tiếng
 \\d+y    \\d+ năm
-??    [??]
 ad    Ấn Độ
+alm   A-La-Hán
 bc    bất cứ
+bcln  bất cứ lúc nào
+bcmc  bất cứ một cái
+bcvg  bất cứ việc gì
 bd    bắt đầu
+bdct  bắt đầu chúng ta
 bg    bao giờ
+bgct  bao giờ chúng ta
+bgct  bây giờ chúng ta
 bgm   bây giờ mình
 blm   ba-la-mật
 bm    biết mình
 bn    bao nhiêu
+bncm  bản ngã của mình
 bt    bản thân
+btct  bình thường chúng ta
+btct  bản thân chúng ta
 btg   bình thường
 bth   bình thường
 btm   bản thân mình
+btmc  bản thân mình chúng
+btmk  bản thân mình không
+btml  bản thân mình là
+btmm  bản thân mình mình
+btmt  bản thân mình thì
+btmv  bản thân mình và
 btr   bên trong
 bv    bởi vì
+bvct  bởi vì chúng ta
 bvl   bởi vì là
 bvs   bởi vì sao
+bvsb  bởi vì sao bởi
 byg   bây giờ
 bđ    bắt đầu
 c1    có một
@@ -35,86 +54,242 @@ c1c   có một cái
 canx  cả những
 cbg   chua bao giờ
 cbi   chuẩn bị
+cbnc  cái bản ngã của
+cbtm  cho bản thân mình
+cbtm  chính bản thân mình
+cbtm  của bản thân mình
 cc    có cái
+ccct  con cái chúng ta
+ccdm  của cuộc đời mình
 ccg   các cảm giác
+ccg   cái cảm giác
+ccgd  cái cảm giác đó
+ccgo  các cảm giác ở
 cch   của chúng
 ccn   có chánh niệm
 cct   của chúng ta
+cctd  cả chúng ta đều
 ccv   của cái việc
 cd    cái điều
 cdc   cuộc đời của
+cdcct cuộc đời của chúng ta
+cdcm  cuộc đời của mình
+cdct  cuộc đời chúng ta
 cdd   cái điều đó
 cdg   cúng dường
+cdgl  chỉ đơn giản là
+cdgl  cái đó gọi là
 cdl   cái đó là
+cdlc  cái đấy là cái
 cdo   cái đó
 cdoi  cuộc đời
+cdolc cái đó là cái
+cdorl cái đó rất là
+cdrl  cái đấy rất là
+cdtt  con đường tu tập
+cdym  cứ để ý mà xem
+cdymx cứ để ý mà xem
 cg    cái gì
 cgc   cảm giác
+cgct  cái gì chúng ta
 cgd   cái gì đó
+cgkc  cảm giác khó chịu
 ch    chẳng hạn
 ch0   chứ không
 chc   chỉ cần
 chch  cho chúng
 chct  cho chúng ta
+chct  chẳng hạn chúng ta
 chk   chánh kiến
 chko  chứ không
 chm   cho mình
 chn   chẳng hạn như
+chnl  chẳng hạn như là
+choct cho chúng ta
 cht   chúng ta
+chtnv chịu trách nhiệm với
+chtnve chịu trách nhiệm về
 cicg  cái cảm giác
 ck    cực kì
+ckct  có khi chúng ta
 ckn   cực kì nhiều
 ckp   chứ không phải
+ckpc  chứ không phải chỉ
+ckpl  chứ không phải là
+ckqt  cực kỳ quan trọng
+cktn  cũng không thể nào
 cl    chất lượng
 cla   chỉ là
+clmc  chỉ là một cái
+clmc  chỉ là một cảm
+clqg  chẳng liên quan gì
 cm    của mình
+cmc   có một cái
+cmcg  có một cảm giác
+cmcg  cứ một cái gì
+cmcgi có một cái gì
+cmct  của mình chúng ta
+cmdv  của mình đối với
 cmn   cả mọi người
+cmno  của mình nó
+cmnt  của mình như thế
+cmsn  có một số người
+cmvs  có một vị Sư
 cn    chánh niệm
+cnbt  con người bình thường
+cncm  cách nhìn của mình
+cnct  chánh niệm chúng ta
+cnct  con người chúng ta
+cncđ  cả những cái đó
 cng   con người
 cnh   cảm nhận
+cnlc  cái này là cái
 cnn   có những người
 cnt   con người ta  
+cntd  cả những thứ đó
 cny   cái này
 cond  con đường
 conx  có những
 coth  có thể
 cr    có rất
 crn   có rất nhiều
+crnn  có rất nhiều người
+crnt  có rất nhiều thứ
+crnđ  có rất nhiều điều
 cs    cuộc sống
 csc   cuộc sống của
+cscc  cuộc sống của chúng
+csch  cuộc sống của họ
+cscm  cuộc sống của mình
+csct  cuộc sống chúng ta
+cscý  có sự chú ý
+cslc  có sự lựa chọn
 ct    cái tâm
 cta   chúng ta
 ctam  cái tâm
 ctb   chúng ta biết
+ctbd  chúng ta bắt đầu
+ctbr  chúng ta biết rằng
+ctc   chúng ta chỉ
 ctc   chúng ta có
+ctc   chúng ta cũng
+ctcc  chúng ta chỉ cần
+ctcc  chúng ta có chánh
+ctcc  chúng ta cũng có
+ctcg  chúng ta cố gắng
+ctck  chúng ta cũng không
+ctcm  chúng ta có một
+ctcm  cái tâm của mình
+ctcn  chúng ta chánh niệm
+ctcn  chúng ta cảm nhận
+ctcn  có thể cảm nhận
+ctcp  chúng ta cũng phải
+ctcp  chúng ta cần phải
 ctcr  chúng ta chỉ
+ctcr  chúng ta có rất
+ctcs  chúng ta có sự
+ctct  chúng ta chúng ta
+ctct  chúng ta có thể
+ctct  chúng ta cũng thế
+ctct  chúng ta cảm thấy
+ctct  cũng thế chúng ta
 ctcx  chúng ta cũng
+ctcy  chúng ta chú ý
 ctd   chúng ta đang
 ctda  chúng ta đã
+ctdac chúng ta đã có
+ctdc  chúng ta đều có
+ctdd  chúng ta đến đây
+ctddc chúng ta đạt được
 ctdeu chúng ta đều
 ctg   chư Tăng
 cth   có thể
+cthd  chúng ta hướng đến
+cthdc chúng ta hiểu được
+cthrl cảm thấy rất là
+ctht  chúng ta hoàn toàn
+ctht  chúng ta hành thiền
 ctk   chúng ta không
+ctkb  chúng ta không biết
+ctkbh chúng ta không bao giờ
+ctkc  chúng ta không cần
+ctkch chúng ta không chịu
+ctkco chúng ta không có
+ctkd  chúng ta không đủ
+ctkh  chúng ta không hiểu
+ctkhe chúng ta không hề
+ctkl  chúng ta không làm
+ctkn  chúng ta không nên
+ctknh chúng ta không nhìn
+ctkp  chúng ta không phải
+ctkt  chúng ta không thể
 ctla  chúng ta là
 ctlai chúng ta lại
 ctlam chúng ta làm
+ctlc  chúng ta lựa chọn
+ctld  có thể làm được
+ctll  chúng ta luôn luôn
+ctlv  chúng ta làm việc
+ctm   chúng ta mới
+ctmb  chúng ta mới bắt
+ctmc  chúng ta mới có
+ctmn  cái tâm mình nó
 ctmoi chúng ta mới
+ctmt  chúng ta mới thấy
 ctn   chúng ta nghĩ
+ctnr  chúng ta nghĩ rằng
+ctnt  chúng ta nhìn thấy
+ctnv  có trách nhiệm với
+ctnve có trách nhiệm về
 ctp   chúng ta phải
+ctpb  chúng ta phải biết
+ctpc  chúng ta phải có
+ctph  chúng ta phải hiểu
+ctpl  chúng ta phải làm
+ctpt  chúng ta phát triển
+ctpts chúng ta phải thực sự
+ctqd  chúng ta quyết định
+ctqs  chúng ta quan sát
 ctr   chúng ta rất
 ctrat chúng ta rất
+ctrl  chúng ta rất là
 cts   chúng ta sẽ
+ctsc  chúng ta sẽ có
+ctsd  chúng ta sử dụng
 ctse  chúng ta sẽ
+ctsk  chúng ta sẽ không
+ctsn  chúng ta suy nghĩ
+ctst  chúng ta sẽ thấy
 ctt   chánh tinh tấn
+ctt   chúng ta thấy
+cttg  chúng ta thư giãn
 ctth  chúng ta thấy
+ctth  chúng ta thực hành
+cttr  chúng ta thấy rất
+cttr  chúng ta thấy rằng
+ctts  chúng ta thực sự
+cttt  chúng ta trở thành
+cttt  chúng ta tu tập
+cttt  các trạng thái tâm
+cttx  chúng ta thường xuyên
+cttx  chúng ta tiếp xúc
+cttđ  chúng ta thay đổi
 ctv   chúng ta vẫn
+ctxd  chúng ta xác định
 cty   cảm thấy
+cuact của chúng ta
+cucm  chú ý của mình
 cv    cái việc
+cvdg  có vấn đề gì
 cvt   chính vì thế
+cvtn  chính vì thế nên
+cvvn  chính vì vậy nên
 cx    cũng
 cxk   cũng không
 cxuc  cảm xúc
 cy    chú ý
+cycct chú ý của chúng ta
+cyhn  cũng y hệt như
 cđ    cuộc đời
 d?    đúng không?
 d?,   đúng không,
@@ -123,308 +298,657 @@ dand  dần dần
 dayl  đấy là
 dc    được
 dcai  được cái
+dcdd  được cái điều đó
+dcgc  được cái gì cả
+dckp  đó chứ không phải
+dclm  đấy cũng là một
 dct   để chúng ta
+dcta  để chúng ta
+dcđ   được cái điều đó
 dd    dễ duôi
 ddao  đắc đạo
+ddct  dần dần chúng ta
+ddct  điều đó chúng ta
 ddd   được điều đó
 ddo   điều đó
+ddrl  điều đó rất là
 dec   để cho
 dect  để chúng ta
 dg    đúng
 dgl   đấy gọi là
 dgn   đơn giản
+dhsc  định hướng sự chú
 dk    điều kiện
+dkct  đôi khi chúng ta
 dkh   đau khổ
+dkpl  đó không phải là
 dl1   đấy là một
 dla   đấy là
 dlc   đó là cái
+dlct  đấy là chúng ta
+dlm   đấy là một
+dlmc  đấy là một cái
+dlnc  đấy là những cái
+dlnn  đấy là những người
+dmln  đến một lúc nào
+dmnt  đến mức như thế
 dmt   đủ mọi thứ
 dndn  dần dần
+dntn  đó như thế nào
 doct  đó chúng ta
 dol   đó là
+dolct đó là chúng ta
+dolmc đó là một cái
+dolnc đó là những cái
+dolnn đó là những người
 dp    Đức Phật
+dpbl  Đức Phật bảo là
+dpct  đa phần chúng ta
+dpmn  đa phần mọi người
+dpnr  đức phật nói rằng
 drl   đó rất là
 dt    đầu tiên
+dtct  đầu tiên chúng ta
+dtdp  dưới thời Đức Phật
 dtg   đối tượng
 dthi  đó thì
+dttm  để trở thành một
 dvay  do vậy
+dvct  do vậy chúng ta
 dvn   do vậy nên
+dvn   được do vậy nên
+dvnc  do vậy nên cái
+dvnct do vậy nên chúng ta
+dvnl  do vậy nên là
+dvnn  đối với những người
 dvoi  đối với
+dymx  để ý mà xem
 dđ    đắc đạo
 gc    gì cả
+gcct  giúp cho chúng ta
 gct   giờ chúng ta
 gd    gì đó
+gict  gì chúng ta
 gl    gọi là
 gn    giống như
+gntt  ghi nhận thuần túy
 gth   giải thích
 gtt   giải thoát
 h?    hiểu không?
 h?,   hiểu không,
 h?.   hiểu không.
+hatt  hình ảnh tự tạo
 hayk  hay không
 hb    hiểu biết
 hd    hành động
 hh    hồi hướng
+hhct  hầu hết chúng ta
+hhmn  hầu hết mọi người
+hhtc  hầu hết tất cả
 hk    hiểu không
 hl    hoặc là
 hla   hoặc là
+hlct  hoặc là chúng ta
 hn    Hà Nội
 hp    hạnh phúc
+hrln  hơn rất là nhiều
+hscy  hướng sự chú ý
 ht    hoàn toàn
+htcm  hết tất cả mọi
 hth   hành thiền
+htkc  hoàn toàn không có
+hđcm  hành động của mình
 k     không
 kb    không biết
 kbg   không bao giờ
+kbgc  không bao giờ chúng
+kbgc  không bao giờ có
+kbgct không bao giờ chúng ta
+kbgd  không bao giờ được
+kbgm  không bao giờ mình
+kbgn  không bao giờ nghĩ
+kbts  không biết tại sao
 kc    không có
 kcc   kích cỡ
+kccg  không có cái gì
+kccn  không có chánh niệm
+kckn  không có khả năng
+kcmc  không có một cái
 kct   khi chúng ta
+kctc  khi chúng ta có
+kctk  khi chúng ta không
+kctl  khi chúng ta làm
+kctt  khi chúng ta thực
+kctts khi chúng ta thực sự
 kd    không được
 kdg   không đúng
 kh    không hiểu
 khkh  khó khăn
 kk    khó khăn
+klqg  không liên quan gì
 km    khi mình
+kmct  khi mà chúng ta
 kn    không nói
+knct  khi nào chúng ta
 ko    không
 kp    không phải
 kpl   không phải là
+kpl1  không phải là một
+kplc  không phải là cái
+kplm  không phải là mình
+kplm  không phải là một
+kplmt không phải là một
+kpnt  không phải như thế
+kptn  không phải tự nhiên
+kptv  không phụ thuộc vào
 kt    không thể
 kth   kích thước
 kthe  không thể
 kti   kiếm tiền
 ktn   không thể nào
+ktnl  không thể nào làm
+ktnm  không thể nào mà
 ktr   kiểm tra
+kytd  không ý thức được
 l1    là một
 l1c   là một cái
 l1n   là một người
+lact  là chúng ta
+lactc là chúng ta cũng
+lactco là chúng ta có
+lactd là chúng ta đang
+lactda là chúng ta đã
+lactk là chúng ta không
+lactp là chúng ta phải
+lacts là chúng ta sẽ
 lbv   là bởi vì
+lbvc  là bởi vì chúng
+lbvm  là bởi vì mình
 lc    lựa chọn
 lcai  là cái
+lcc   làm cho chúng
+lcct  làm cho chúng ta
 lcg   là cái gì
 lcho  làm cho
 lcm   làm cho mình
+lcnt  làm cho người ta
 lco   là có
 lct   là chúng ta
+lct   làm chúng ta
+lctc  làm chúng ta cũng
+lctco làm chúng ta có
+lctd  làm chúng ta đang
+lctda làm chúng ta đã
+lctk  làm chúng ta không
+lctp  làm chúng ta phải
+lcts  làm chúng ta sẽ
 ld    làm được
+ldcdp lời dạy của Đức Phật
+ldct  lúc đó chúng ta
+lddd  làm được điều đó
+ldll  lặp đi lặp lại
 ldo   lúc đó
+ldđ   làm được điều đó
 lg    là gì
 lii   lợi ích
 lk    là không
+lkbg  là không bao giờ
+lkct  là khi chúng ta
 ll    luôn luôn
 lm    là mình
+lmc   là một cái
+lmcd  là một cách để
+lmcg  là một cái gì
+lmcg  là một cảm giác
+lmcn  là một con người
+lmn   là một người
+lmnr  là một người rất
 lmp   là mình phải
 ln    là nó
 lnao  lúc nào
+lnct  là nhiều chúng ta
+lnct  lúc nào chúng ta
 lng   là người
 lnh   là nhiều
 lnhu  là như
+lnmc  lúc nào mình cũng
 lnn   là những người
 lnt   là như thế
+lntn  là như thế nào
+lntn  là như thế này
 lnx   là những
 lp    làm phước
 lpcd  làm phước cúng dường
+lqgd  liên quan gì đến
 lqt   là quan trọng
 ls    làm sao
+lscy  là sự chú ý
 lsm   làm sao mà
 lt    lập tức
+ltnd  làm thế nào để
+ltnt  liên tục như thế
 lv    làm việc
 m     mình
 mak   mà không
 mb    mình biết
 mc    mà chúng
 mcai  một cái
+mccn  mình có chánh niệm
 mcg   một cái gì
+mcgc  một cái gì cả
+mcgd  một cái gì đó
 mch   mà chúng
+mcha  một cái hình ảnh
+mcht  một cách hoàn toàn
+mckp  mình chứ không phải
+mclm  một cái là mình
 mco   mình có
+mcrl  một cách rất là
+mcrn  mình có rất nhiều
+mct   mà chúng ta
 mct   mình có thể
 mcta  mà chúng ta
+mctc  mà chúng ta có
+mctc  mà chúng ta cứ
+mctk  mà chúng ta không
+mctl  mình có thể làm
+mctn  một cách tự nhiên
+mctp  mà chúng ta phải
+mctt  một cách trực tiếp
+mctv  mà chúng ta vẫn
+mcvt  một cách vô thức
 mcx   mình cũng
+mcđg  một cách đơn giản
+md    Miến Điện
 md    mình đang
+mdnl  một điều nữa là
+mdvn  mình do vậy nên
 mk    mình không
 mkb   mình không biết
+mkbg  mình không bao giờ
+mkct  mà khi chúng ta
+mkhb  mình không hề biết
+mktn  mình không thể nào
 ml    mà lại
 mla   mình là
 mlam  mình làm
+mlmn  mình là một người
+mlnd  một lúc nào đó
 mm    mà mình
 mme   mạnh mẽ
 mmk   mà mình không
+mmkb  mà mình không biết
+mmkh  mà mình không hề
+mnbt  một người bình thường
 mnd   mọi người đều
 mng   mọi người
+mngd  mọi người đều
+mngnd một người nào đó
+mnnd  một ngày nào đó
 mno   mình nó
 mnt   mình như thế
+mntn  mình như thế nào
 mp    mình phải
 mph   mình phải
+mrln  mình rất là nhiều
 ms    mình sẽ
 mt    mình thấy
 mtc   một cách
+mtct  mình thì chúng ta
+mtdh  một tiếng đồng hồ
 mtg   một thời gian
+mtgl  một thời gian là
+mtgs  một thời gian sau
+mtgt  một thời gian thì
 mthu  mọi thứ
+mtnc  một tí nào cả
 mtng  một người
 mts   một số
 mtth  một thời
+mttm  mình trở thành một
+mttt  một trạng thái tâm
 n     nhưng
 nc    nước
+nccn  người có chánh niệm
+ncdl  những cái đó là
 ncg   những cái gì
+ncgm  những cái gì mình
+ncgma những cái gì mà
+nclm  nó chỉ là một
 ncn   nói chuyện
 nco   nó có
 nct   nên chúng ta
+nctc  nào chúng ta cũng
+nctd  nó có tác dụng
+nctk  nên chúng ta không
+nctp  nên chúng ta phải
+nctt  người có trí tuệ
 ncx   nó cũng
 nd    nào đó
 ng    người
+ngct  những gì chúng ta
 ngd   người đó
 ngdo  người đó
+ngmd  những gì mình đang
 ngt   người ta
 ngta  người ta
+ngtd  những gì thầy dạy
 ngth  ngồi thiền
 nh    nhưng
 nhla  nhất là
 nhm   nhưng mà
 nhmc  nhưng mà chúng
+nhmct nhưng mà chúng ta
+nhmkc nếu mà không có
+nhmkp nhưng mà không phải
+nhmtr nhưng mà thực ra
+nhmts nhưng mà tại sao
 nhng  nhiều người
 nhth  nhìn thấy
 nhula như là
+nkbg  nó không bao giờ
 nkh   người khác
+nkpl  nó không phải là
+nktn  nó không thể nào
 nla   nên là
+nlcc  nó làm cho chúng
+nlcg  nó là cái gì
+nlcm  nó làm cho mình
+nlct  nhất là chúng ta
+nlct  nên là chúng ta
 nlm   nó là một
+nlmc  nó là một cái
+nlnt  nhiều lần như thế
 nlt   ngay lập tức
 nm    nhưng mà
 nma   nếu mà
+nmbv  nhưng mà bởi vì
 nmc   nếu mà chúng
+nmct  nếu mà chúng ta
+nmkc  nếu mà không có
+nnct  này nọ các thứ
+nndl  như nó đang là
 nng   nhiều người
+nngnt những người như thế
+nnnt  những người như thế
+nntn  nó như thế nào
 nok   nó không
 nola  nó là
+nptv  nó phụ thuộc vào
 nr    nó rất
 nrl   nó rất là
 ns    nó sẽ
 nt    như thế
 nta   người ta
+ntbv  như thế bởi vì
 ntc   như thế chúng
+ntcm  người thân của mình
+ntct  người ta có thể
+ntct  như thế chúng ta
 ntd   những thứ đó
+ntgl  người ta gọi là
 ntk   người ta không
+ntlc  như thế là chúng
 ntn   như thế nào
+ntnn  như thế này này
 ntno  như thế nào
 ntny  như thế này
 nts   nên tại sao
+ntsm  nên tại sao mà
 ntt   như thế thì
+nttm  như thế thì mình
+nttm  nó trở thành một
+ntts  như thế tại sao
+nttt  những trạng thái tâm
+nuctk nếu chúng ta không
 nv    như vậy
+nvcct nhiệm vụ của chúng ta
+nvcm  nhiệm vụ của mình
+nvct  như vậy chúng ta
 nvl   như vậy là
 nx    những
 nxc   những cái
+nxlp  ngài Xá-Lợi-Phất
 nxng  những người
 nxth  những thứ
+nđct  nào đó chúng ta
+obmd  ở bên Miến Điện
 od    ở đây
+odct  ở đây chúng tabccg  bất cứ cái gì
 otr   ở trong
+ottm  ở trong tâm mình
 p     Phật
 pb    phước báu
 pc    phước
+pgnt  Phật Giáo nguyên thủy
 ph    Pháp
 phc   phước
 phuoc phước
 pl    phải là
 pn    phiền não
 pt    phát triển
+ptcn  phát triển chánh niệm
+pttl  phát triển tâm linh
+pttt  phát triển trí tuệ
 pv    phục vụ
+qalh  quả A-La-Hán
 qd    quyết định
 qs    quan sát
 qt    quan trọng
+qtlv  quay trở lại với
+qtnl  quan trọng nhất là
+qttt  quá trình tu tập
 quyd  quy định
+rdmt  rồi đủ mọi thứ
 rl    rất là
+rlbc  rất là buồn cười
+rldd  rất là dễ dàng
+rldg  rất là đơn giản
 rlk   rất là khó
+rlmm  rất là mạnh mẽ
 rln   rất là nhiều
+rlnh  rất là nguy hiểm
+rlqt  rất là quan trọng
+rlrr  rất là rõ ràng
 rn    rất nhiều
 rnn   rất nhiều người
 rnng  rất nhiều người
+rnnl  rất nhiều năng lượng
 rnt   rất nhiều thứ
+rntg  rất nhiều thời gian
 rnth  rất nhiều thứ
+rnth  rất nhiều trường hợp
+rntk  rất nhiều thứ khác
 rr    rõ ràng
+rscn  rà soát cảm nhận
+sccd  suốt cả cuộc đời
 scm   sống của mình
 scy   sự chú ý
 sd    sử dụng
 sm    sao mà
+smct  sao mà chúng ta
+smtg  sau một thời gian
 sn    suy nghĩ
+sncm  suy nghĩ của mình
+snnt  suy nghĩ như thế
 sp    số phận
 st    sự thật
+stkl  sự tự kỷ luật
 t     Tăng
 t1    thành một
 tak   tà kiến
+taktn ta không thể nào
 tamct tâm chúng ta
 tamm  tâm mình
 tath  ta thấy
 tb    Tam Bảo
 tc    tất cả
 tca   tất cả
+tccta tất cả chúng ta
 tcm   tất cả mọi
 tcmn  tất cả mọi người
+tcmt  tất cả mọi thứ
+tcmv  tất cả mọi việc
 tcn   tất cả những
+tcnc  tất cả những cái
+tcnd  tất cả những điều
+tcng  tất cả những gì
+tcnt  tất cả những thứ
 tco   ta có
+tcrn  ta có rất nhiều
 tcs   trong cuộc sống
 tct   ta có thể
+tctc  thì chúng ta có
+tctc  thì chúng ta cũng
+tctk  thì chúng ta không
+tctl  ta có thể làm
+tctl  thì chúng ta lại
+tctm  thì chúng ta mới
+tctp  thì chúng ta phải
+tcts  thì chúng ta sẽ
+tctt  thì chúng ta thấy
 tcx   ta cũng
 td    thay đổi
+tdcc  thái độ chân chánh
+tdcm  thái độ của mình
+tdct  trong đầu chúng ta
+tdlt  thay đổi liên tục
+tdrl  thay đổi rất là
 tg    thời gian
+tgkm  thư giãn khuôn mặt
+tgtm  thư giãn thoải mái
 tgx   thư giãn
-th    thầy
+th    Thầy
 thc   thành công
 thcg  thành công
 thch  thậm chí
+thcm  thực hành của mình
+thcn  thực hành chánh niệm
+thct  thực hành chúng ta
+thd   thay đổi
 thect thế chúng ta
 thh   thực hành
 thict thì chúng ta
 thim  thì mình
 thl   thì là
 thm   thân mình
+thmc  thực hành một cách
 thn   thế nào
 thnao thể nào
 thnen thế nên
 thno  thì nó
+thnt  thực hành như thế
 thny  thế này
 thsu  thực sự
 tht   thông tin
 thte  thực tế
+thth  thông thường
 thth  thế thì
 tk    ta không
+tkbg  ta không bao giờ
+tkbg  thì không bao giờ
+tknt  ta không nhìn thấy
+tktn  thì không thể nào
 tl    tức là
+tlct  thường là chúng ta
+tlct  tức là chúng ta
 tmai  thoải mái
+tmct  ta mới có thể
 tmoi  ta mới
+tmtg  tu một thời gian
+tmtq  thành một thói quen
 tn    tự nhiên
 tnao  thế nào
+tnct  thế nên chúng ta
 tnh   thế nhưng
 tnl   thế nên là
+tnlct thế nên là chúng ta
 tnm   thế nhưng mà
+tnmct thế nhưng mà chúng ta
 tnt   tính toán
+tntk  thế này thế kia
+tntn  thế này thế nọ
+tnts  thế nên tại sao
 tp    ta phải
+tpth  ta phải thực hành
 trc   trước
+trhnt trường hợp như thế
 trtg  truyền thống
 trth  truyền thừa
 ts    tại sao
+tsct  tại sao chúng ta
 tse   ta sẽ
 tsm   tại sao mình
+tsmk  tại sao mình không
 tst   ta sẽ thấy
 tsth  ta sẽ thấy
+tstr  ta sẽ thấy rằng
 tt    tinh tấn
 tt1   trở thành một
 ttam  trong tâm
-tth   trở thành
+ttcm  trí tuệ của mình
+ttct  thế thì chúng ta
+trtct trong tâm chúng ta
+ttdn  từ trước đến nay
+trth  trở thành
+tth   tình thương
 ttj   tu tập
+ttlc  thu thúc lục căn
 ttm   trong tâm mình
+ttmn  trở thành một người
 ttmt  trở thành một
+ttnl  tiêu tốn năng lượng
 ttp   tu tập
+tttb  trạng thái tâm bất
+tttg  trí tuệ trực giác
 ttue  trí tuệ
+ttvt  từ trong vô thức
 tuh   tu hành
+tvcm  thật với chính mình
+tvmn  Thầy và mọi người
+tvtm  thân và tâm mình
+tvtr  thở vào thở ra
 vact  và chúng ta
+vactk và chúng ta không
+vactt và chúng ta thấy
+vakct và khi chúng ta
+vbtm  với bản thân mình
+vcqt  vô cùng quan trọng
 vct   vì chúng ta
+vctp  vậy chúng ta phải
 vd    vấn đề
 vde   vấn đề
+vdgc  vấn đề gì cả
+vdk   vô điều kiện
 vdn   ví dụ như
 vdnl  ví dụ như là
+vdtl  việc đầu tiên là
 vdu   ví dụ
+vebtm về bản thân mình
 vg    việc gì
+vgct  việc gì chúng ta
+vict  vì chúng ta
+victk vì chúng ta không
+victt vì chúng ta thấy
+vlct  vậy là chúng ta
 vm    và mình
+vmnt  về mình như thế
 vn    vậy nên
+vnct  vậy nên chúng ta
 vq    vượt qua
+vqtn  việc quan trọng nhất
 vt    vì thế
 xd    xứng đáng
 yhn   y hệt như
+yhnt  y hệt như thế
 yo    yếu ớt
 đ     đúng đắn
+đctm  đó chúng ta mới
+đctp  đó chúng ta phải
+đgnt  đơn giản như thế
+đgpx  đánh giá phán xét
+đv1n  đối với một người
+đvct  đối với chúng ta
+đvmn  đối với mọi người
+đvmtn đối với một người
 đđ    đúng đắn`
